7c369374727c1cc7db39be6969257e0d8844b1f7
Ualikhan Bisenev <imperialsnakes1200@gmail.com>
2026-07-08 11:29:46 +0500

    update context_processors

--- templates/context_processors.py
+++ templates/context_processors.py
@@ -1,11 +1,18 @@
-# templates/context_processors.py
-
 def project_global_info(request):
     """
     Injects global metadata context into all rendering pipelines 
     to fulfill advanced evaluation criteria.
     """
-    return {
+    context = {
         'site_version': '1.0.2',
-        'team_name': 'gorePisatel'
+        'team_name': 'gorePisatel',
+        'current_theme': 'light',
     }
+
+    if request.user.is_authenticated:
+        from apps.users.models import UserSettings
+
+        user_settings, _ = UserSettings.objects.get_or_create(user=request.user)
+        context['current_theme'] = user_settings.theme
+
+    return context
